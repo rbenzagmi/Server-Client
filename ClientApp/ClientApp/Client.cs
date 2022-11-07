217620ae170aa729dce76d3b3dfefd6a2ca9960d
Roni Ben Zagmi <rbenzagmi@gmail.com>
2022-11-07 19:19:31 +0200

    Add files via upload

---
+++ ClientApp/ClientApp/Client.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Media.Imaging;
+using System.Windows;
+
+namespace ClientApp
+{
+    class Client
+    {
+        public static Socket client; // Create new socket
+
+        private static string server_ip;
+
+        private static byte[] bufferRec;
+
+        private static byte[] bufferSend = new byte[1024];
+
+        private static int port;
+
+        private static IPEndPoint addr;
+
+        static MainWindow mainWin;
+
+        public Client(MainWindow win, string serverIp, int serverPort)
+        // Open the client socket 
+        {
+            try
+            {
+                mainWin = win;
+                client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // Open new client socket
+                server_ip = serverIp;
+                port = serverPort;
+                addr = new IPEndPoint(IPAddress.Parse(server_ip), port);
+            }
+            catch
+            {
+                MessageBox.Show("Error with socket of the client");
+            }
+
+        }
+
+        public void Open()
+        // Open thread of the requests from the server
+        {
+            Thread tr1 = new Thread(new ThreadStart(RequestsFromTheServer));
+            tr1.Start();
+
+        }
+
+        private void RequestsFromTheServer()
+        // The method works in own thread and listening to requests from the server
+        {
+            try
+            {
+                client.Connect(addr); // The connection of the client
+            }
+            catch
+            {
+                MessageBox.Show("Error with the connection of the client");
+            }
+
+            try
+            {
+                bufferRec = new byte[1024];
+                string receivedBytes = "";
+                receivedBytes = client.Receive(bufferRec).ToString(); // Recieve request from the server
+                string rec = "";
+                rec = System.Text.Encoding.ASCII.GetString(bufferRec); // The request
+                string newRec = string.Empty;
+                if (rec.IndexOf("\0") != -1)
+                {
+                    newRec = rec.Substring(0, rec.IndexOf("\0")); // Substring the two words
+                }
+                else
+                    newRec = rec;
+
+                string nameOfPic = newRec.Split('@')[0]; // The name of the image from the request
+
+                mainWin.Dispatcher.Invoke((Action)(() =>
+                {
+                    mainWin.image.Source = new BitmapImage(new Uri(@"/Images/" + nameOfPic, UriKind.Relative)); // Put the image in the client window
+                }));
+
+                if (newRec.Split('@')[1] == "move") // If the second word is this word so the client need to move
+                {
+                    mainWin.StartMovingThePic();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error with recieve of the client");
+            }
+
+
+
+        }
+
+        internal void Finished()
+        // Send to the server that the image finish to move
+        {
+            try
+            {
+                bufferSend = System.Text.Encoding.ASCII.GetBytes("Finished");
+                client.Send(bufferSend, SocketFlags.None); // Send to the server that the client finish
+            }
+            catch
+            {
+                MessageBox.Show("Error with send of the client");
+            }
+
+        }
+    }
+}
---
+++ ClientApp/ClientApp/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Client client;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            connectToServer();
+            
+        }
+
+        private void connectToServer()
+        // The connection to the server
+        {
+            try
+            {
+                client = new Client(this, "192.168.56.1", 40000);
+                client.Open();
+            }
+            catch
+            {
+                MessageBox.Show("Error with the client");
+            }
+
+        }
+
+        public void animat_Completed(object sender, EventArgs e)
+        // When the image finish to move in the client window
+        {
+            client.Finished();
+
+            try
+            {
+                this.Dispatcher.Invoke((Action)(() =>
+                {
+
+                    ThicknessAnimation animat = new ThicknessAnimation();
+                    animat.From = new Thickness(canvas.Width, 0, 0, 0); // The start 
+                    animat.To = new Thickness(canvas.Width+canvas.Width, 0, 0, 0); // The end
+                    animat.Duration = new Duration(TimeSpan.FromSeconds(2)); // The time
+                    image.BeginAnimation(Image.MarginProperty, animat); // Start to move
+                }));
+            }
+            catch
+            {
+                MessageBox.Show("Error with the move");
+            }
+        }
+
+        public void StartMovingThePic()
+        // The move of the picture in the client window
+        {
+            try
+            {
+                this.Dispatcher.Invoke((Action)(() =>
+                {
+
+                    ThicknessAnimation animat = new ThicknessAnimation();
+                    animat.From = new Thickness(0, 0, 0, 0); // The start 
+                    animat.To = new Thickness(canvas.Width, 0, 0, 0); // The end
+                    animat.Duration = new Duration(TimeSpan.FromSeconds(2)); // The time
+                    animat.Completed += animat_Completed;
+                    image.BeginAnimation(Image.MarginProperty, animat); // Start to move
+                }));
+            }
+            catch
+            {
+                MessageBox.Show("Error with the move of the picture");
+            }
+
+        }
+    }
+}
